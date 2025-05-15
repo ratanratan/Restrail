8d6d07d1e9eef2ae401c811d8deeffe43d831f08
ratan <ratanitproject@gmail.com>
2025-05-15 16:37:18 +0530

    data

--- src/main/java/com/sathya/as/EmployeeController.java
+++ src/main/java/com/sathya/as/EmployeeController.java
@@ -3,6 +3,7 @@ package com.sathya.as;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/emps")
+@CrossOrigin(origins = "*")
 public class EmployeeController {
 
     @Autowired
